931b07d2a1f85918dd46f36f809f174e7c148327
jackcizon <jack20021213cn@gmail.com>
2023-10-01 23:05:30 +0800

    algo

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -17,22 +17,22 @@
 '''
 
 
+def max_heapify(arr, end, start):
+    left = 2 * start + 1
+    right = 2 * start + 2
+    # define a pointer _max to point max_value_index
+    max_val_index = start
 
-def max_heapify(arr, size, i):
-    l = 2 * i + 1
-    r = 2 * i + 2
+    if left < end and arr[left] > arr[max_val_index]:
+        max_val_index = left
 
-    _max = i
+    if right < end and arr[right] > arr[max_val_index]:
+        max_val_index = right
 
-    if l < size and arr[l] > arr[_max]:
-        _max = l
-
-    if r < size and arr[r] > arr[_max]:
-        _max = r
-
-    if _max != i:
-        arr[i], arr[_max] = arr[_max], arr[i]
-        max_heapify(arr, size, _max)
+    #if pointer changed, swap and heapify the child
+    if max_val_index != start:
+        arr[start], arr[max_val_index] = arr[max_val_index], arr[start]
+        max_heapify(arr, end, max_val_index)
 
 
 def build_max_heap(arr):
@@ -43,24 +43,53 @@ def build_max_heap(arr):
 def heap_sort(arr):
     build_max_heap(arr)
     size = len(arr) - 1
-    l = len(arr) - 1
+    end = len(arr) - 1
     for i in range(size, -1, -1):
         arr[0], arr[i] = arr[i], arr[0]
-        l -= 1
-        max_heapify(arr, l, 0)
-    print(arr)
+        end -= 1
+        max_heapify(arr, end, 0)
+
 
 def main():
 
     arr = [3, 5, 2, -99, 133, 789, 0, 1, 56, 1, 7, 9, 6]
-
     heap_sort(arr)
-
-
+    print(arr)
 
 
 if __name__ == '__main__':
     main()
+
+
+'''
+we do not need to build maxc heap from last element in array, beause all leaves are max_heap
+and we heapify from size//2 downto 1, to ensure after all heapify can make max_heap
+
+build_max_heap:
+                  789
+                /    |
+             133      9
+            /  |     / |
+          56   7    6  0
+         /|   /|   /| /|
+        1 99 1 5  2 3 <<==size, end
+
+after first sort:
+                   3 <<== arr[0]
+                /    |
+             133        9
+            /  |     /   |
+          56   7    6    0
+         /|   /|   /|   /|
+        1 99 1 5  2 789 
+                 /\\
+                 ||
+              new end, size
+
+heapify, sort, etc ...
+
+sort need n rounds, each round get a relative large_value, from largest to smallest
+'''
 
 
 
