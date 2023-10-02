8752f5bbcc256a358cf733c64440047a9d88b000
jackcizon <jack20021213cn@gmail.com>
2023-10-02 13:45:35 +0800

    algo

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,37 +1,30 @@
 def partition(arr, low, high):
     pivot = arr[high]
     i = low
-
     for j in range(low, high):
-        
         """
         i <= j, arr[i] >= arr[j], pivot is virtual pivot
         """
         if arr[j] < pivot:
             arr[i], arr[j] = arr[j], arr[i]
             i += 1
-
     arr[i], arr[high] = arr[high], arr[i] 
 
     return i
 
 
 def quick_sort(arr, low, high):
-
     if low < high:
         pivot = partition(arr, low, high)
-
         quick_sort(arr, low, pivot - 1)
-
         quick_sort(arr, pivot + 1, high)
 
 
 def main():
     arr = [10, 7, 8, 9, 1, 5, 4, 4, 2, 1, 0, -7, 4]
-
     quick_sort(arr, 0, len(arr) - 1)
-
     print(arr)
+
 
 if __name__ == '__main__':
     main() 
