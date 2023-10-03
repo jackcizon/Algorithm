47c430e71a60ae76a4c0b5b5f820872c1a4a694b
jackcizon <jack20021213cn@gmail.com>
2023-10-03 17:56:51 +0800

    update bf

--- brute_force/closest_pointer.py
+++ brute_force/closest_pointer.py
@@ -5,33 +5,31 @@ class Point():
         self.x = x
         self.y = y
 
-class Distance(Point):
+class Distance():
+    @staticmethod
     def distance(p1, p2):
         return math.sqrt(
-            (p1.x - p2.x) *(p1.x - p2.x) 
-            + (p1.y - p2.y) * (p1.y - p2.y)
+            (p1.x - p2.x) ** 2 +
+            (p1.y - p2.y) ** 2
         )
 
-
-class BruteForce(Distance):
+class BruteForce():
+    @staticmethod
     def min_len(P, size):
         min_val = float('inf')
 
         for i in range(size):
-
             for j in range(i + 1, size):
-                
                 if Distance.distance(P[i], P[j]) < min_val:
                     min_val = Distance.distance(P[i], P[j])
         
         return min_val
 
-
 P = [
-        Point(2, 3), Point(12, 30),
-        Point(40, 50), Point(5, 1),
-        Point(12, 10), Point(3, 4)
-    ]
-
+    Point(2, 3), Point(12, 30),
+    Point(40, 50), Point(5, 1),
+    Point(12, 10), Point(3, 4)
+]
 
 print("smallest distance:", BruteForce.min_len(P, len(P)))
+
