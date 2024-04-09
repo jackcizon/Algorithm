fa280366181755866fed2a0c592c6083f1641646
jackcizon <jack20021213cn@gmail.com>
2024-04-09 12:02:09 +0800

    add a comment to bubble_sort.py

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,3 +1,9 @@
+'''Bubble Sort
+1. The list is divided into ordered area and unordered area. 
+2. For every two adjacent numbers in the list, if the former is larger than the latter, swap the two numbers. 
+3. After one sorting pass is completed, the number of unordered areas is reduced by one and the number of ordered areas is increased by one.
+4. Starting from the area with index 0, the number of each trip is reduced by one.
+'''
 def bubble_sort(array):
     for i in range(len(array) - 1):
     
